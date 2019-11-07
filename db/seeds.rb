4aaa756a111c128378c7aae04a60301a57b72395
moustapha cisse <ctapha13@gmail.com>
2019-11-07 18:56:50 +0100

    rails-mister-cocktail

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+
+puts 'Cleaning db ingredient'
+Ingredient.destroy_all
+
+puts'seeding db'
+puts '=========='
+puts 'parsing js file'
+
+records = JSON.parse(open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read)
+ingredients = records['drinks']
+ingredients.each do |ingredient|
+  Ingredient.create(name: ingredient['strIngredient1'])
+end
+
+Cocktail.create(name: "Gin")
+Cocktail.create(name: "Whisky")
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "BissapMix")
+Cocktail.create(name: "GingembreRel")
+
+#Dose.create(cocktail: cocktail1, ingredient: ingredient1, description: "Gin Tonic with lemons")
+#Dose.create(cocktail: cocktail2, ingredient: ingredient2, description: "Whisky with ice")
+
+puts 'finished!'
